442274ed5904b2961ea369175691a525c7b179d4
SergSerg <sergio.ne@inbox.ru>
2020-06-29 18:18:51 +0600

    Добавил конструктор в MainPresenter.

--- Lesson 1 MVP Moxy/MVPonly/app/src/main/java/com/example/mvponly/presenter/MainPresenter.java
+++ Lesson 1 MVP Moxy/MVPonly/app/src/main/java/com/example/mvponly/presenter/MainPresenter.java
@@ -1,11 +1,20 @@
 package com.example.mvponly.presenter;
 
-import com.example.mvponly.view.MainActivity;
+import android.util.Log;
+
+import com.example.mvponly.model.Model;
+import com.example.mvponly.view.MainView;
 
 public class MainPresenter {
     private static final String TAG = "MainPresenter";
+    private Model model;
+    private MainView mainView;
 
-
-    public MainPresenter(MainActivity mainActivity) {
+    public MainPresenter(MainView mainView) {
+        Log.d(TAG, "MainPresenter: ");
+        this.mainView = mainView;
+        model = new Model();
     }
+
+
 }
