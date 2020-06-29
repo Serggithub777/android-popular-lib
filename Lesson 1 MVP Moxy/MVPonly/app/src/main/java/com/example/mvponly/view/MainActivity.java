dd9eb482830ee37fc97a25cda5c40e94bb5cb751
SergSerg <sergio.ne@inbox.ru>
2020-06-29 17:53:07 +0600

    Создал класс Model. Переменную стринг, геттер и сеттер.

--- Lesson 1 MVP Moxy/MVPonly/app/src/main/java/com/example/mvponly/view/MainActivity.java
+++ Lesson 1 MVP Moxy/MVPonly/app/src/main/java/com/example/mvponly/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.mvponly;
+package com.example.mvponly.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.mvponly.R;
 
 public class MainActivity extends AppCompatActivity {
 
