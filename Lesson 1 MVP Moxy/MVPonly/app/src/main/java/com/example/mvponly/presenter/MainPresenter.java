eef9cfc0383342b3124f0665a2c17810d9d58bd0
SergSerg <sergio.ne@inbox.ru>
2020-06-29 18:12:01 +0600

    Добавил инициализацию элементов макета в MainActivity.

--- Lesson 1 MVP Moxy/MVPonly/app/src/main/java/com/example/mvponly/presenter/MainPresenter.java
+++ Lesson 1 MVP Moxy/MVPonly/app/src/main/java/com/example/mvponly/presenter/MainPresenter.java
@@ -1,7 +1,11 @@
 package com.example.mvponly.presenter;
 
+import com.example.mvponly.view.MainActivity;
+
 public class MainPresenter {
     private static final String TAG = "MainPresenter";
 
 
+    public MainPresenter(MainActivity mainActivity) {
+    }
 }
--- Lesson 1 MVP Moxy/MVPonly/app/src/main/java/com/example/mvponly/view/MainActivity.java
+++ Lesson 1 MVP Moxy/MVPonly/app/src/main/java/com/example/mvponly/view/MainActivity.java
@@ -3,15 +3,37 @@ package com.example.mvponly.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.mvponly.R;
+import com.example.mvponly.presenter.MainPresenter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainView{
     private static final String TAG = "MainActivity";
+    private EditText enteredText;
+    private TextView textAdded;
+    private Button buttonAddText;
+    private MainPresenter mainPresenter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: ");
+        enteredText = findViewById(R.id.enteredText);
+        textAdded = findViewById(R.id.textViewAdded);
+        buttonAddText = findViewById(R.id.buttonAdd);
+
+        mainPresenter = new MainPresenter(this);
+
+    }
+
+    @Override
+    public void setAddedText(String text) {
+
     }
 }
