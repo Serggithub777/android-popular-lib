b4f831cacbfdeb517a56ae5fc8d5fbd8aeb24b55
SergSerg <sergio.ne@inbox.ru>
2020-06-29 22:14:44 +0600

    Создал класс Presenter c применением moxy.

--- Lesson 1 MVP Moxy/MVPwithMoxy/app/src/main/java/com/example/mvpwithmoxy/presenter/Presenter.java
+++ Lesson 1 MVP Moxy/MVPwithMoxy/app/src/main/java/com/example/mvpwithmoxy/presenter/Presenter.java
@@ -1,4 +1,34 @@
 package com.example.mvpwithmoxy.presenter;
 
-public class Presenter {
+import android.util.Log;
+
+import com.example.mvpwithmoxy.model.Model;
+import com.example.mvpwithmoxy.view.MainView;
+
+import moxy.MvpPresenter;
+
+public class Presenter extends MvpPresenter<MainView> {
+    private static final String TAG = "SecondPresenter";
+    private Model model;
+
+    public Presenter() {
+        Log.d(TAG, "Presenter: ");
+        this.model = new Model();
+    }
+
+    @Override
+    protected void onFirstViewAttach() {
+        super.onFirstViewAttach();
+        Log.d(TAG, "onFirstViewAttach: ");
+    }
+    private String appendStr(String text, String str) {
+        return text + str;
+    }
+
+    public void onButtonClick(String str) {
+        String text = model.getText();
+        String newText = appendStr(text, str);
+        model.setText(newText);
+        getViewState().setAddedText(newText);
+    }
 }
